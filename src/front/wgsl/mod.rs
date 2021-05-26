424bf72075d023df3e01a50d1af41ceac3166150
Jim Blandy <jimb@red-bean.com>
2021-05-26 16:05:42 -0700

    [wgsl-in]: Use a `matches!` expression for brevity.

--- src/front/wgsl/mod.rs
+++ src/front/wgsl/mod.rs
@@ -1232,12 +1232,11 @@ impl Parser {
         mut handle: Handle<crate::Expression>,
         allow_deref: bool,
     ) -> Result<Handle<crate::Expression>, Error<'a>> {
-        let mut needs_deref = match ctx.expressions[handle] {
-            crate::Expression::LocalVariable(_) | crate::Expression::GlobalVariable(_) => {
-                allow_deref
-            }
-            _ => false,
-        };
+        let mut needs_deref = allow_deref
+            && matches!(
+                ctx.expressions[handle],
+                crate::Expression::LocalVariable(_) | crate::Expression::GlobalVariable(_)
+            );
         loop {
             // insert the E::Load when we reach a value
             if needs_deref {
